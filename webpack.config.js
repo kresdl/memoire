6181dc51fb1ffdaba01967ba09a8f8935d3c8fdf
krsdl <35543755+Kresdl@users.noreply.github.com>
2020-05-10 11:38:35 +0200

    react-presets fix

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,7 @@ module.exports = ({ development: dev = false } = {}) => {
             loader: 'babel-loader',
             options: {
               presets: [
-                !dev && [
+                [
                   '@babel/preset-env',
                   {
                     targets: '> 0.25%, not dead',
